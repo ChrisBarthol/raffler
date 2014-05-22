fa95c033f362baa458960f950613a27bb437ff83
ChrisBarthol <Chris.Barthol@gmail.com>
2014-05-22 16:27:51 -0400

    ember toy app, a raffle, can add names

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Raffler::Application.config.secret_key_base = 'e89819c6071817c12538ef12d8474f9d50cb90e24043830b8cfd9a2b7c86048e604f8bbd654d2c92e8554c25311fe915dc716fb268f1794d9e095ccd215784f2'
